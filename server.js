75a3b015cff7b26fee4f48dd96fd9d41a710137c
Saadatres5 <saadat.res5@gmail.com>
2026-06-03 18:12:21 +0400

    Update server.js

--- server.js
+++ server.js
@@ -766,7 +766,6 @@ app.get('/sitemap.xml', function (req, res) {
     res.writeHead(200, {
       'Content-Type':  'application/xml; charset=utf-8',
       'Cache-Control': 'public, max-age=3600',
-      'X-Robots-Tag':  'noindex',
     });
     res.end(xml);
   } catch (e) {
